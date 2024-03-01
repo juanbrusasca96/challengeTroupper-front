471654badb521cfe85097f7a9658749582ab7ca6
Juan Brusasca <juanbrusasca96@gmail.com>
2024-03-01 09:12:38 -0300

    first commit

---
+++ src/views/pages/home/ViewHome.jsx
@@ -0,0 +1,14 @@
+import { Grid, Typography } from '@mui/material'
+import React from 'react'
+
+const ViewHome = () => {
+  return (
+    <div>
+        <Grid container sx={{margin: 10}}>
+        <Typography variant='overline'>Home</Typography>
+        </Grid>
+    </div>
+  )
+}
+
+export default ViewHome
